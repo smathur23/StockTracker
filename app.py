2738327f682deda7eebe16258cb4b6838aa67c6f
smathur23 <saahilmathur@icloud.com>
2024-06-20 00:42:03 -0400

    Added percent and automatic price updates

--- app.py
+++ app.py
@@ -25,18 +25,32 @@ def get_price(symbol):
         print(f"Error fetching stock data: {e}")
         return 0
 
+def pct_change(symbol):
+    try:
+        stock = yf.Ticker(symbol)
+        history = stock.history(period='2d')
+        if len(history) < 2:
+            raise ValueError("Not enough data to calculate percent change")
+        
+        close_prices = history['Close']
+        pct_change = ((close_prices.iloc[-1] - close_prices.iloc[-2]) / close_prices.iloc[-2]) * 100
+        return round(pct_change, 2)
+    except Exception as e:
+        print(f"Error fetching stock data: {e}")
+        return 0
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ticker = db.Column(db.String(10), nullable=False)
-    recent_price = db.Column(db.Integer, default=0)
+    recent_price = db.Column(db.Float, default=0.0)  # Change to db.Float
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    percent_change = db.Column(db.Float, default=0.0)
 
     def __repr__(self):
         return '<Stock %r>' % self.id 
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
-    
     alert_flag = ""
 
     if request.method == 'POST':
@@ -46,29 +60,34 @@ def home():
         existing_stock = Todo.query.filter_by(ticker=stock_name).first()
         if existing_stock:
             alert_flag = "Error with existing stock."
-            try:
-                stocks = Todo.query.order_by(Todo.date_created).all()
-                return render_template('index.html', stocks=stocks, alert_flag=alert_flag)
-            except Exception as e:
-                return f'Error updating existing stock: {e}'
         else:
-            new_stock = Todo(ticker=stock_name)
-
             try:
                 price = get_price(stock_name)
-                if (price == 0):
-                    stocks = Todo.query.order_by(Todo.date_created).all()
-                    return render_template('index.html', stocks=stocks, alert_flag="Stock does not exist!")
-                new_stock.recent_price = price
-                db.session.add(new_stock)
-                db.session.commit()
-                return redirect('/')
+                if price == 0:
+                    alert_flag = "Stock does not exist!"
+                else:
+                    pct = pct_change(stock_name)
+                    new_stock.percent_change = pct
+                    new_stock.recent_price = price
+                    db.session.add(new_stock)
+                    db.session.commit()
+                    return redirect('/')
             except Exception as e:
-                return f'Error adding new stock: {e}'
-    else:
-        stocks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', stocks=stocks, alert_flag=alert_flag)
+                alert_flag = f"Error adding new stock: {e}"
+
+    # Update prices for all stocks
+    stocks = Todo.query.order_by(Todo.date_created).all()
+    for stock in stocks:
+        try:
+            stock.recent_price = get_price(stock.ticker)
+            stock.percent_change = pct_change(stock.ticker)
+        except Exception as e:
+            print(f'Error updating price for {stock.ticker}: {e}')
     
+    db.session.commit()
+
+    return render_template('index.html', stocks=stocks, alert_flag=alert_flag)
+
 @app.route('/email_prices', methods=['POST', 'GET'])
 def email_prices():
     if request.method == 'POST':
@@ -79,7 +98,7 @@ Subject: Financial Data
         """
         message += f'\n'
         for stock in stocks:
-            message += f'Most Recent Price of {stock.ticker}: {stock.recent_price}\n'
+            message += f'Most Recent Price of {stock.ticker}: {stock.recent_price} with {stock.percent_change}% change\n'
         try:
             context = ssl.create_default_context()
             with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
@@ -94,7 +113,6 @@ Subject: Financial Data
         stocks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', stocks=stocks, alert_flag="")
 
-
 @app.route('/delete/<int:id>')
 def delete(id):
     stock_to_delete = Todo.query.get_or_404(id)
@@ -105,21 +123,6 @@ def delete(id):
         return redirect('/')
     except:
         return 'Error in deleting stock'
-    
-
-@app.route('/update_prices')
-def update_prices():
-    stocks = Todo.query.all()
-
-    for stock in stocks:
-        try:
-            stock.recent_price = get_price(stock.ticker)
-        except Exception as e:
-            print(f'Error updating price for {stock.ticker}: {e}')
-
-    db.session.commit()
-    return redirect('/')
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
